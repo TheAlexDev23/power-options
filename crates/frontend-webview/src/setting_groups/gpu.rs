4600f2da62d8dc9e481348a018869840468daf0c
TheAlexDev23 <thealexdev23@gmail.com>
2024-09-21 18:43:16 +0200

    Added AMD GPU settings to the webview frontend

--- crates/frontend-webview/src/setting_groups/gpu.rs
+++ crates/frontend-webview/src/setting_groups/gpu.rs
@@ -1,20 +1,24 @@
 use std::time::Duration;
 
 use dioxus::prelude::*;
-use power_daemon::{GpuSettings, ProfilesInfo, ReducedUpdate, SystemInfo};
+use power_daemon::{AmdGpuInfo, GpuSettings, ProfilesInfo, ReducedUpdate, SystemInfo};
 
 use crate::communication_services::{
     control_routine_send_multiple, ControlAction, ControlRoutine, SystemInfoRoutine,
     SystemInfoSyncType,
 };
-use crate::helpers::toggleable_components::ToggleableNumericField;
-use crate::helpers::toggleable_types::ToggleableInt;
+use crate::helpers::toggleable_components::{ToggleableDropdown, ToggleableNumericField};
+use crate::helpers::toggleable_types::{ToggleableInt, ToggleableString};
 
 #[derive(Debug, Clone, PartialEq, Default)]
 struct GpuForm {
     pub intel_min: ToggleableInt,
     pub intel_max: ToggleableInt,
     pub intel_boost: ToggleableInt,
+
+    pub amd_perf_level: ToggleableString,
+    pub amd_perf_state: ToggleableString,
+    pub amd_power_profile: ToggleableString,
 }
 
 impl GpuForm {
@@ -28,6 +32,13 @@ impl GpuForm {
         self.intel_min.from_u32(gpu_settings.intel_min);
         self.intel_max.from_u32(gpu_settings.intel_max);
         self.intel_boost.from_u32(gpu_settings.intel_boost);
+
+        self.amd_perf_level
+            .from(gpu_settings.amd_dpm_perf_level.clone());
+        self.amd_perf_state
+            .from(gpu_settings.amd_dpm_power_state.clone());
+        self.amd_power_profile
+            .from(gpu_settings.amd_power_profile.clone());
     }
 }
 
@@ -51,7 +62,39 @@ pub fn GpuGroup(
         .gpu_settings
         .clone();
 
-    let intel_gpu_supported = system_info().unwrap().gpu_info.intel_info.is_some();
+    let system_info = system_info().unwrap();
+
+    let intel_gpu_supported = system_info.gpu_info.intel_info.is_some();
+
+    let mut supports_amd_gpu = false;
+    let mut supports_amd_perf_level = false;
+    let mut supports_amd_power_state = false;
+    let mut supports_amd_power_profiles = false;
+
+    if let Some(ref info) = system_info.gpu_info.amd_info {
+        supports_amd_gpu = true;
+        if matches!(info, AmdGpuInfo::AmdGpu { dpm_perf: _ }) {
+            supports_amd_perf_level = true;
+            supports_amd_power_state = false;
+            supports_amd_power_profiles = false;
+        }
+        if matches!(
+            info,
+            AmdGpuInfo::Radeon {
+                dpm_perf: _,
+                dpm_state: _
+            }
+        ) {
+            supports_amd_perf_level = true;
+            supports_amd_power_state = true;
+            supports_amd_power_profiles = false;
+        }
+        if matches!(info, AmdGpuInfo::Legacy { power_profile: _ }) {
+            supports_amd_perf_level = false;
+            supports_amd_power_state = false;
+            supports_amd_power_profiles = true;
+        }
+    }
 
     let mut form_used_settings = use_signal(|| gpu_settings.clone());
     let mut form = use_hook(|| GpuForm::new(&gpu_settings));
@@ -85,9 +128,21 @@ pub fn GpuGroup(
             } else {
                 None
             },
-            amd_dpm_perf_level: todo!(),
-            amd_dpm_power_state: todo!(),
-            amd_power_profile: todo!(),
+            amd_dpm_perf_level: if supports_amd_gpu && supports_amd_perf_level {
+                form.amd_perf_level.into_base()
+            } else {
+                None
+            },
+            amd_dpm_power_state: if supports_amd_gpu && supports_amd_power_state {
+                form.amd_perf_state.into_base()
+            } else {
+                None
+            },
+            amd_power_profile: if supports_amd_gpu && supports_amd_power_profiles {
+                form.amd_power_profile.into_base()
+            } else {
+                None
+            },
         };
 
         control_routine_send_multiple(
@@ -149,6 +204,60 @@ pub fn GpuGroup(
                             None
                         },
                         value: form.intel_boost
+                    }
+                }
+            }
+
+            div { class: "option-group",
+                div { class: "option",
+                    ToggleableDropdown {
+                        name: labels::AMD_GPU_PERF_LEVEL,
+                        value: form.amd_perf_level,
+                        items: vec!["low".to_string(), "auto".to_string(), "high".to_string()],
+                        disabled: !supports_amd_gpu || !supports_amd_perf_level,
+                        tooltip: if !supports_amd_gpu {
+                            Some(labels::AMD_GPU_MISSING_TT.to_string())
+                        } else if !supports_amd_perf_level {
+                            Some(labels::AMD_GPU_PERF_LEVEL_UNAVAILABLE.to_string())
+                        } else {
+                            Some(labels::AMD_GPU_PERF_LEVEL_TT.to_string())
+                        }
+                    }
+                }
+                div { class: "option",
+                    ToggleableDropdown {
+                        name: labels::AMD_GPU_STATE,
+                        value: form.amd_perf_state,
+                        items: vec!["battery".to_string(), "balanced".to_string(), "performance".to_string()],
+                        disabled: !supports_amd_gpu || !supports_amd_power_state,
+                        tooltip: if !supports_amd_gpu {
+                            Some(labels::AMD_GPU_MISSING_TT.to_string())
+                        } else if !supports_amd_power_state {
+                            Some(labels::AMD_GPU_STATE_UNAVAILABLE.to_string())
+                        } else {
+                            Some(labels::AMD_GPU_STATE_TT.to_string())
+                        }
+                    }
+                }
+                div { class: "option",
+                    ToggleableDropdown {
+                        name: labels::AMD_GPU_POWER_PROFILE,
+                        value: form.amd_power_profile,
+                        items: vec![
+                            "low".to_string(),
+                            "mid".to_string(),
+                            "high".to_string(),
+                            "auto".to_string(),
+                            "default".to_string(),
+                        ],
+                        disabled: !supports_amd_gpu || !supports_amd_power_profiles,
+                        tooltip: if !supports_amd_gpu {
+                            Some(labels::AMD_GPU_MISSING_TT.to_string())
+                        } else if !supports_amd_power_profiles {
+                            Some(labels::AMD_GPU_POWER_PROFILE_UNAVAILABLE.to_string())
+                        } else {
+                            Some(labels::AMD_GPU_POWER_PROFILE_TT.to_string())
+                        }
                     }
                 }
             }
