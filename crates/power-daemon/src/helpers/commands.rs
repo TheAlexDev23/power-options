6ad0e302ee7db94949dbaa1fa5275063434e6983
TheAlexDev23 <thealexdev23@gmail.com>
2024-09-24 09:25:07 +0200

    Got rid of subshells entirely in command-running helpers

--- crates/power-daemon/src/helpers/commands.rs
+++ crates/power-daemon/src/helpers/commands.rs
@@ -14,19 +14,21 @@ pub fn command_exists(command: &str) -> bool {
 
 pub fn run_command(command: &str) {
     debug!("running: {command}");
-    let output = Command::new("sh")
-        .args(["-c", command])
-        .stdout(Stdio::inherit())
-        .stderr(Stdio::inherit())
+
+    let mut command = get_command_from_string(command);
+
+    let output = command
         .spawn()
-        .expect("Could not run command")
+        .expect("Could not spawn command")
         .wait_with_output()
         .expect("Could not wait command");
 
-    trace!(
-        "Command output: {}",
-        String::from_utf8(output.stdout).unwrap()
-    );
+    if !output.stdout.is_empty() {
+        trace!(
+            "Command output: {}",
+            String::from_utf8(output.stdout).unwrap()
+        );
+    }
     if !output.stderr.is_empty() {
         error!(
             "Command returned with stderr: {}",
@@ -40,13 +42,12 @@ pub fn run_command(command: &str) {
 pub fn run_command_with_output(command: &str) -> (String, String) {
     trace!("getting output of: {command}");
 
-    let mut command_proc = Command::new("sh");
-    command_proc.args(["-c", command]);
+    let mut command = get_command_from_string(command);
 
-    let result = command_proc.output().expect("Could not run command");
+    let output = command.output().expect("Could not run command");
 
-    let stdout = String::from_utf8_lossy(&result.stdout).to_string();
-    let stderr = String::from_utf8_lossy(&result.stderr).to_string();
+    let stdout = String::from_utf8_lossy(&output.stdout).to_string();
+    let stderr = String::from_utf8_lossy(&output.stderr).to_string();
 
     trace!("Output is: {stdout} {stderr}");
 
@@ -56,8 +57,7 @@ pub fn run_command_with_output(command: &str) -> (String, String) {
 pub fn run_graphical_command(command: &str) {
     debug!("running graphical command: {command}");
 
-    let output = Command::new("sh")
-        .args(["-c", command])
+    let output = get_command_from_string(command)
         .stdout(Stdio::inherit())
         .stderr(Stdio::inherit())
         .env("DISPLAY", ":0")
@@ -81,14 +81,24 @@ pub fn run_graphical_command(command: &str) {
 
 pub fn run_graphical_command_in_background(command: &str) -> std::process::Child {
     debug!("running graphical command in background: {command}");
-    Command::new("sh")
-        .args(["-c", command])
+
+    get_command_from_string(command)
         .stdout(Stdio::inherit())
         .stderr(Stdio::inherit())
         .env("DISPLAY", ":0")
         .env("XAUTHORITY", get_xauthority())
         .spawn()
         .expect("Could not run command")
+}
+
+fn get_command_from_string(command: &str) -> Command {
+    let parts = shellwords::split(command).expect("Could not parse command parts");
+    let (cmd, args) = parts
+        .split_first()
+        .expect("Could split first of arguments vector");
+    let mut command = Command::new(cmd);
+    command.args(args);
+    command
 }
 
 fn get_xauthority() -> String {
